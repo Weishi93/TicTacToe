eef4df6031b68ad4e5a54dea1a5f34068586ce6f
weishi93 <ws1196@nyu.edu>
2017-05-02 11:57:01 -0400

    Add AISearch

--- src/tictactoe/GameBoard.java
+++ src/tictactoe/GameBoard.java
@@ -1,12 +1,15 @@
 package tictactoe;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Created by Wei Shi on 5/1/17.
  */
 public class GameBoard {
-    private char[][] board;
+    protected char[][] board;
     private static int SIZE = 4;
     protected int cellLeft = SIZE * SIZE;
     public GameBoard() {
@@ -74,5 +77,88 @@ public class GameBoard {
             }
         }
         return win;
+    }
+
+    public List<int[]> getAvailableCell(char[][] newBoard) {
+        List<int[]> result = new ArrayList<>();
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (newBoard[row][col] == '~') {
+                    result.add(new int[]{row, col});
+                }
+            }
+        }
+        return result;
+    }
+
+    public int[] ABSearch(char[][] board) {
+        ReturnType result = Max_Value(board, 1000, -1000, 0);
+        return result.point;
+    }
+
+    public ReturnType Max_Value(char[][] board, int a, int b, int depth) {
+        if (checkWin('X')) {
+            return new ReturnType(null, 1000);
+        }
+        if (checkWin('O')) {
+            return new ReturnType(null, -1000);
+        }
+        List<int[]> avaliableCell = getAvailableCell(board);
+        if (avaliableCell.isEmpty()) {
+            return new ReturnType(null, 0);
+        }
+        int value = Integer.MIN_VALUE;
+        for (int[] point : avaliableCell) {
+            char[][] newBoard = getNewBoard(board, point, 'X');
+            value = Math.max(value, Min_Value(newBoard, a, b, depth + 1).value);
+            if (value >= b) {
+                return new ReturnType(point, value);
+            }
+            a = Math.max(a, value);
+        }
+        return new ReturnType(null, value);
+    }
+
+    public ReturnType Min_Value(char[][] board, int a, int b, int depth) {
+        if (checkWin('X')) {
+            return new ReturnType(null, 1000);
+        }
+        if (checkWin('O')) {
+            return new ReturnType(null, -1000);
+        }
+        List<int[]> avaliableCell = getAvailableCell(board);
+        if (avaliableCell.isEmpty()) {
+            return new ReturnType(null, 0);
+        }
+        int value = Integer.MAX_VALUE;
+        for (int[] point : avaliableCell) {
+            char[][] newBoard = getNewBoard(board, point, 'O');
+            value = Math.min(value, Max_Value(newBoard, a, b, depth + 1).value);
+            if (value >= a) {
+                return new ReturnType(point, value);
+            }
+            a = Math.max(a, value);
+        }
+        return new ReturnType(null, value);
+    }
+
+    public char[][] getNewBoard(char[][] board, int[] point, char player) {
+        char[][] newBoard = new char[SIZE][SIZE];
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                newBoard[row][col] = board[row][col];
+            }
+        }
+        newBoard[point[0]][point[1]] = player;
+        return newBoard;
+    }
+}
+
+class ReturnType {
+    int[] point;
+    int value;
+    public ReturnType(int[] point, int value) {
+        this.point = point;
+        this.value = value;
     }
 }
--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -22,20 +22,31 @@ public class TicTacToe {
         } else {
             curtPlayer = 'X';
         }
+        // take turns to play game
         while (gb.cellLeft > 0) {
-            System.out.println("Player " + curtPlayer + " Please input your row and col");
-            int row = sc.nextInt();
-            int col = sc.nextInt();
+            if (curtPlayer == 'O') { // player's turn
+                System.out.println("Player " + curtPlayer + " Please input your row and col");
+                int row = sc.nextInt();
+                int col = sc.nextInt();
 
-            // if this move is valid
-            boolean moveSuccess = gb.makeMove(curtPlayer, row, col);
-            if (!moveSuccess) {
-                System.out.println("Your input is not valid, please input again!");
-            } else {
+                // if this move is valid
+                boolean moveSuccess = gb.makeMove(curtPlayer, row, col);
+                if (!moveSuccess) {
+                    System.out.println("Your input is not valid, please input again!");
+                } else {
+                    if (gb.checkWin(curtPlayer)) break;
+                    curtPlayer = 'X';
+                }
+                gb.displayBoard();
+                System.out.println();
+            } else { // AI's turn
+                int[] res = gb.ABSearch(gb.board);
+                gb.board[res[0]][res[1]] = 'X';
                 if (gb.checkWin(curtPlayer)) break;
-                curtPlayer = curtPlayer == 'O' ? 'X' : 'O';
+                curtPlayer = 'O';
+                gb.displayBoard();
             }
-            gb.displayBoard();
+
         }
         if (gb.cellLeft == 0) {
             if (gb.checkWin('X')) {
@@ -46,6 +57,7 @@ public class TicTacToe {
                 System.out.println("Draw!");
             }
         } else {
+            gb.displayBoard();
             System.out.println(curtPlayer + " has won!");
         }
     }
